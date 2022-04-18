9ff4a8d229747615fca4506b8a528f0922e77a5c
mirzokhidkhasanov <mirzohidkhasanov12@gmail.com>
2022-04-18 17:16:32 +0500

    RabbitMQMessageProducer was added

--- amqp/src/main/java/com/mk/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/mk/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.mk.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
@@ -23,8 +24,18 @@ public class RabbitMQConfig {
         return rabbitTemplate;
     }
 
+
     @Bean
-    public MessageConverter jacksonConverter(){
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+
+    @Bean
+    public MessageConverter jacksonConverter() {
         MessageConverter jackson2JsonMessageConverter =
                 new Jackson2JsonMessageConverter();
         return jackson2JsonMessageConverter;
